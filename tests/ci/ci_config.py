2fa7cee2c67d6d6eb998f371aefd5eed90f7e48f
Max Kainov <max.kainov@clickhouse.com>
2025-01-24 18:27:17 +0100

    set from cache report url

--- tests/ci/ci_config.py
+++ tests/ci/ci_config.py
@@ -595,6 +595,7 @@ class CI:
                 exclude_files=[".md"],
                 docker=["clickhouse/fasttest"],
             ),
+            run_command="fast_test_check.py",
             timeout=2400,
             runner_type=Runners.BUILDER,
         ),
